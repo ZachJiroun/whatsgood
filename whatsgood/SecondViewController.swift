cb62cf2fed38d9a791830c8225998bfcb3e1b985
Yoshi Fujimoto <yosh.fuj@gmail.com>
2015-01-17 22:39:06 -0500

    Radius exit keyboard fixed

--- whatsgood/SecondViewController.swift
+++ whatsgood/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var radiusField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,12 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        radiusField.resignFirstResponder()
+    }
+    
+    
 
 
 }
---
+++ whatsgood/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  What's Good?
+//
+//  Created by Yoshio Fujimoto on 1/17/15.
+//  Copyright (c) 2015 Zach Jiroun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
